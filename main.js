930421fde6a580b4c1e217385783ab1354b36808
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-09-10 17:36:22 -0300

    Ajustando e corrigindo erros

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const nav = document.querySelector("nav");
-const label = document.querySelector("label");
 const meowSound = new Audio("./assets/meow-cat.mp3");
 const button = document.querySelector(".open-menu");
 
 const changeSize = () => {
   nav.classList.toggle("active");
+  nav.style.transition = ".7s";
   meowSound.play();
-  nav.style.transition = ".9s";
+
+  const buttonExpanded = button.getAttribute("aria-expanded", "aria-label");
+
+  const buttonLabel = button.getAttribute("aria-label");
+  button.setAttribute("aria-expanded", "true");
+  button.setAttribute("aria-label", "open");
+
+  if (buttonExpanded === "true" && buttonLabel === "open") {
+    button.setAttribute("aria-expanded", "false");
+    button.setAttribute("aria-label", "fechado");
+  }
 };
 
 button.addEventListener("click", changeSize);
-label.addEventListener("click", changeSize);
